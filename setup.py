df4c5641a60b1386ad9067c3667ed153cd513e8f
Himani Agrawal <himani93@gmail.com>
2018-03-11 15:09:54 +0530

    setup build

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+setup(name='Availability Slots',
+      version='0.1',
+      description='Availability Slots - Given a weekly schedule provide available slots from a given time',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords='healthifyme availability slots',
+      author='Himani Agrawal',
+      author_email='himani93@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      tests_require=['pytest'],
+      setup_require=['pytest-runner'],
+      include_package_data=True,
+      entry_points={
+          'console_scripts': ['availabile-slots=available_slots.main:main'],
+      },
+      zip_safe=False)
